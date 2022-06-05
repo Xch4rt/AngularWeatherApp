406d67636ea3ca4cdb5c54794a4b32c76e998a5d
Xch4rt <pab203.guti@gmail.com>
2022-06-05 00:12:50 -0600

    Proyecto Finalizado

---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, withLatestFrom } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { WeatherData } from '../models/weather.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+
+  constructor(private http: HttpClient) { }
+
+  getWeatherData(cityName: string) :Observable<WeatherData>{
+    return this.http.get<WeatherData>(environment.weatherApiBaseURL, {
+      headers: new HttpHeaders()
+      .set(environment.XRapidAPIHostHeaderName, environment.XRapidAPIHostHeaderValue)
+      .set(environment.XRapidAPIKeyHeaderName, environment.XRapidAPIKeHeaderValue),
+      params: new HttpParams()
+      .set('q', cityName)
+      .set('units', 'metric')
+      .set('mode', 'json')
+    })
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,13 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
-};
-
+  production: false,
+  weatherApiBaseURL: 'https://community-open-weather-map.p.rapidapi.com/weather',
+  XRapidAPIHostHeaderValue: 'community-open-weather-map.p.rapidapi.com',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeHeaderValue: '7fd17c9a3dmsh76cdf5c47141161p1257f3jsn57f0522e4d95'
+}
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
@@ -13,4 +17,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI
